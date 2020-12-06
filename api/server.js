f052f553803c01bd8e472aef324ac9e4dc08f01b
freakMax <devmaxfreak@gmail.com>
2020-12-06 16:28:11 +0200

    refactoring

--- api/server.js
+++ api/server.js
@@ -1,14 +1,13 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT
+const config = require('./config.js')
 
 app.get('/',(req,res)=>res.send('Main page'));
 app.get('/error',(req,res) => res.send(error()))
-app.get('/getToken',(req,res) => res.send(process.env.TOKEN))
 
 app.use((err,req,res,next) => {
     console.log(err.stack);
     res.status(500).send('500 server error')
 })
 
-app.listen(PORT,()=> console.log(`Hello.Im working at ${PORT} port`));
+app.listen(config.PORT,()=> console.log(`Hello.Im working at ${config.PORT} port`));
