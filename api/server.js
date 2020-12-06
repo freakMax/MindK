aed4ede92bf10063f3fc1f478c5b6f7f7ce53a09
freakMax <devmaxfreak@gmail.com>
2020-12-06 13:42:17 +0200

    add .env and add error handler

--- api/server.js
+++ api/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT
+
+app.get('/',(req,res)=>res.send('Main page'));
+app.get('/error',(req,res) => res.send(error()))
+app.get('/getToken',(req,res) => res.send(process.env.TOKEN))
+
+app.use((err,req,res,next) => {
+    console.log(err.stack);
+    res.status(500).send('500 server error')
+})
 
 app.listen(PORT,()=> console.log(`Hello.Im working at ${PORT} port`));
-app.get('/',(req,res)=>res.send('Im from GET'));
