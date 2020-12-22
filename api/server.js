5d5e4bc7b29fa763cdde2230772a526a2bc9305d
freakMax <devmaxfreak@gmail.com>
2020-12-22 12:19:59 +0200

    add routes for posts

--- api/server.js
+++ api/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const config = require('./config.js')
 const PORT = config.getPort('PORT',3000)
+const postsRouter = require('./routes/posts.router');
 
-app.get('/',(req,res)=>res.send('Main page'));
-app.get('/error',(req,res) => res.send(error()))
+app.use('/posts', postsRouter);
 
 app.use((err,req,res,next) => {
     console.log(err.stack);
     res.status(500).send('500 server error')
 })
+
 
 app.listen(PORT,()=> console.log(`Hello.Im working at ${PORT} port`));
