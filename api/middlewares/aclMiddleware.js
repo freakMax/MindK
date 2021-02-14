7fbf8414b928138e4536099a53518eea871d1f61
freakMax <devmaxfreak@gmail.com>
2021-02-14 12:47:25 +0200

    add acl

--- api/middlewares/aclMiddleware.js
+++ api/middlewares/aclMiddleware.js
@@ -1,26 +1,37 @@
 const db = require('../services/db')
+const permissions = require('../services/permissions')
 
-
-
-module.exports = function(table,column){
-    return async function (req,res,next){
+module.exports = function (limits){
+    return async function(req,res,next){
+        let isHavePermission = false
         const userId = req.user
         const postId = req.params.id
-        const posts = await db.query(`SELECT ${column} FROM ${table} WHERE id = $1`,[postId])
-        const role = await db.query('SELECT * FROM users WHERE id = $1',[userId])
-        console.log(posts.rows)
-        console.log(role.rows)
-        if(role.rows[0].role == 'admin'){
-            res.send('Администратор')
-            next()
-        }else{
-            if(posts.rowCount && posts.rows[0].user_id == userId){
-                res.send('Удаленно')
-                next()
-            }else{
-                res.send('Ты не создавал этот пост!')
+        const role = await db.query('SELECT role FROM users WHERE id = $1',[userId])
+        const currentPermissions = permissions[role.rows[0].role]
+        limits.forEach(async(limit) => {
+            if(limit.userPost){
+                const post = await db.query(`SELECT ${limit.userPost.column} FROM ${limit.userPost.table} WHERE id = $1`,[postId])
+                if(post.rows[0].user_id == userId){
+                    console.log('owner')
+                    isHavePermission = true
+                    if(isHavePermission){
+                        next()
+                    }else{
+                        console.log('not your')
+                    }
+                }else{
+                    if(currentPermissions.includes(limit.permission)){
+                        console.log('adm')
+                        isHavePermission = true
+                        if(isHavePermission){
+                            next()
+                        }else{
+                            console.log('not your')
+                        }
+                    }
+                }
             }
-        }
-        }
+        });
     }
+}
 
--- api/routes/posts.router.js
+++ api/routes/posts.router.js
@@ -7,7 +7,13 @@ const aclMiddleware = require('../middlewares/aclMiddleware')
 router.post('/', authMiddleware,postsController.addPost);
 router.get('/', postsController.getAllPost);
 router.get('/:id', postsController.getOnePost);
-router.put('/:id', [authMiddleware,aclMiddleware('posts','user_id')],postsController.updatePost);
-router.delete('/:id', [authMiddleware,aclMiddleware('posts','user_id')],postsController.deletePost);
+router.put('/:id', [authMiddleware,aclMiddleware([
+    { permission: 'updateAnyPost' },
+    { permission: 'updateOwnPost', userPost: {table: 'posts', column: 'user_id'}}])],
+    postsController.updatePost);
+router.delete('/:id', [authMiddleware,aclMiddleware([
+    { permission: 'deleteAnyPost' },
+    { permission: 'deleteOwnPost', userPost: {table: 'posts', column: 'user_id'}}])],
+    postsController.deletePost);
 
 module.exports = router; 
