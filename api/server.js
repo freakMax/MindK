c59faebcc155731fadd67b7ec7e1eca9b3612aef
freakMax <devmaxfreak@gmail.com>
2021-02-20 11:11:26 +0200

    create api files

--- api/server.js
+++ api/server.js
@@ -1,19 +1,5 @@
 const express = require('express');
 const app = express();
-const config = require('./config.js')
-
-app.get('/',(req,res)=>res.send('Main page'));
-app.get('/error',(req,res) => res.send(error()))
-
-app.use((err,req,res,next) => {
-    console.log(err.stack);
-    res.status(500).send('500 server error')
-})
-
-app.listen(config.PORT,()=> console.log(`Hello.Im working at ${config.PORT} port`));
-  
-const express = require('express');
-const app = express();
 const config = require('./config')
 const PORT = config.getValue('PORT')
 const postsRouter = require('./routes/posts.router');
