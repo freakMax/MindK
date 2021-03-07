40f6328a65bd4359ce98d182ee221c02de07713c
freakMax <devmaxfreak@gmail.com>
2021-03-07 19:25:36 +0200

    add validator

--- api/routes/posts.router.js
+++ api/routes/posts.router.js
@@ -3,13 +3,21 @@ const router = new Router();
 const postsController = require('../controller/posts.controller')
 const authMiddleware = require('../middlewares/authMiddleware')
 const aclMiddleware = require('../middlewares/aclMiddleware')
+const validator = require('../middlewares/validator')
 
-router.post('/', authMiddleware,postsController.addPost);
+router.post('/', [authMiddleware,validator({
+    title:['required','min:1','max:30'],
+    content:['required','min:1','max:300'],
+    access:[['oneOf:me:friends:all']]})],postsController.addPost);
 router.get('/', postsController.getAllPost);
 router.get('/:id', postsController.getOnePost);
 router.put('/:id', [authMiddleware,aclMiddleware([
     { permission: 'updateAnyPost' },
-    { permission: 'updateOwnPost', ownerInfo: {table: 'posts', column: 'user_id'}}])],
+    { permission: 'updateOwnPost', ownerInfo: {table: 'posts', column: 'user_id'}}],
+    validator({
+        title:['required','min:1','max:30'],
+        content:['required','min:1','max:300'],
+        access:[['oneOf:me:friends:all']]}))],
     postsController.updatePost);
 router.delete('/:id', [authMiddleware,aclMiddleware([
     { permission: 'deleteAnyPost' },
